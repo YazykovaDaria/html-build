77885dd9f97d36495bdf409d5edcb6530c068971
YazykovaDaria <yazykovadasva@gmail.com>
2022-07-22 15:56:15 +0300

    change model with classes

--- src/js/model.js
+++ src/js/model.js
@@ -1,13 +1,39 @@
+import image from '../assets/img/bag.jpg';
+import { TextBlock, TitleBlock, ImageBlock, ColumnsBlock } from '../classes/block';
+
 const model = [
-  { type: 'title', value: 'Hello JS' },
-  { type: 'text', value: 'text here' },
-  {
-    type: 'columns',
-    value: [
-      '111111111', '22222222', '333333333',
-    ],
-  },
-  { type: 'img', value: './assets/img/bag.jpg' },
+  new TitleBlock('Hello JS', {
+    tag: 'h2',
+    styles: {
+      background: 'linear-gradient(to right, #ff0099, #493240)',
+      padding: '1.5rem',
+      'text-align': 'center',
+    },
+  }),
+  new TextBlock('text here', {
+    styles: {
+      padding: '1.5rem',
+      'text-align': 'center',
+    },
+  }),
+  new ColumnsBlock([
+    '111111111', '22222222', '333333333',
+  ], {
+    styles: {
+      'text-align': 'center',
+    },
+  }),
+  new ImageBlock(image, {
+    styles: {
+      display: 'flex',
+      'justify-content': 'center',
+    },
+    imageStyles: {
+      width: '400px',
+      height: 'auto',
+    },
+    alt: 'мем',
+  }),
 ];
 
 export default model;
