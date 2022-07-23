e7123931af0039fefd2c7ef9f6b283969ae20d9a
YazykovaDaria <yazykovadasva@gmail.com>
2022-07-23 12:53:29 +0300

    change structure, add App

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import model from './js/model';
-import {
-  getColumns, getImg, getText, getTitle,
-} from './js/templates';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/main.css';
+import App from './classes/app';
 
-const SITE = document.getElementById('site');
-
-model.forEach((block) => {
-  let html = '';
-  if (block.type === 'title') {
-    html = getTitle(block);
-  } else if (block.type === 'text') {
-    html = getText(block);
-  } else if (block.type === 'columns') {
-    html = getColumns(block);
-  } else if (block.type === 'img') {
-    html = getImg(block);
-  }
-
-  SITE.insertAdjacentHTML('beforeend', html);
-});
+new App(model).init();
