29107f8de4e2b59ee5e27dbb0988f4dc1a4bcfec
YazykovaDaria <yazykovadasva@gmail.com>
2022-07-23 12:53:44 +0300

    add App

---
+++ src/classes/app.js
@@ -0,0 +1,23 @@
+import Sidebar from './sidebar';
+import Site from './site';
+
+class App {
+  constructor(model) {
+    this.model = model;
+  }
+
+  init() {
+    const SITE = new Site('#site');
+
+    SITE.render(this.model);
+
+    const updateCollback = (newBlock) => {
+      this.model.push(newBlock);
+      SITE.render(this.model);
+    };
+
+    new Sidebar('#panel', updateCollback);
+  }
+}
+
+export default App;
