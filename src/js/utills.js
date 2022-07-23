1eec273b888405b5bbaf7678018b06341d7692c2
YazykovaDaria <yazykovadasva@gmail.com>
2022-07-23 12:56:14 +0300

    add block

--- src/js/utills.js
+++ src/js/utills.js
@@ -3,6 +3,25 @@ export const row = (content, styles = '') => `<div class="row" style="${styles}"
 export const col = (content) => `<div class="col-sm">${content}</div>`;
 
 export const css = (styles) => {
+  //разобраться с добавлением стилей!!! строки из формы не приходят в нужном виде
+  console.log(styles);
+  if (typeof styles === 'string') {
+    return styles;
+  }
   const toSring = (key) => `${key}: ${styles[key]}`;
   return Object.keys(styles).map(toSring).join(';');
 };
+
+export const block = (type) => `
+  <form name="${type}">
+  <h5>${type}</h5>
+  <div class="form-group">
+  <input class="form-control form-control-sm" name="value" placeholder="тип элемента">
+  </div>
+  <div class="form-group">
+  <input class="form-control form-control-sm" name="styles" placeholder="стили элемента">
+  </div>
+  <button type="submit" class="btn btn-primary btn-sm">Добавить</button>
+  </form>
+  <hr />
+  `;
